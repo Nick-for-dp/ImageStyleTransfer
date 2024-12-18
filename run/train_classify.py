ca4c2fe744da6392e701fb186e4822bf1cf7f5c6
nick <kehanwu1997@163.com>
2024-12-18 22:01:16 +0800

    Update train script with pretrain model load

--- run/train_classify.py
+++ run/train_classify.py
@@ -9,16 +9,32 @@ from torch.utils.data import DataLoader
 from torch.optim.lr_scheduler import CyclicLR
 
 
-def train(model, epochs, device, save_root_path, batch_size=8, learning_rate=2e-3):
+def train(model, epochs, device, save_root_path, pre_train_model_path, batch_size=8, learning_rate=1e-2):
     if not os.path.exists(save_root_path):
         os.makedirs(save_root_path, exist_ok=True)
+
+    if pre_train_model_path is not None:
+        pretrain_model_param = torch.load(pre_train_model_path)
+        translate_pre_train_model_param = {}
+        for k in pretrain_model_param.keys():
+            if k.startswith('features'):
+                new_k = k.replace('features', 'conv_net')
+                translate_pre_train_model_param[new_k] = pretrain_model_param.get(k, None)
+            elif k.startswith('classifier'):
+                continue
+            else:
+                translate_pre_train_model_param[k] = pretrain_model_param.get(k, None)
+        model_dict = model.state_dict()  # 原模型参数
+        model_dict.update(translate_pre_train_model_param)  # 更新原模型的部分参数，除去classifier那部分
+        model.load_state_dict(model_dict)
+
     train_dataset = CIFARDataset(root_path='../train_data/cifar_10', is_train=True)
     train_dataloader = DataLoader(train_dataset, batch_size=batch_size)
     lr = learning_rate  # 学习率
     criterion = nn.CrossEntropyLoss().to(device)
     optimizer = optim.Adam(model.parameters(), lr=lr)
     # 学习率随着训练进行不断调整
-    scheduler = CyclicLR(optimizer, base_lr=1e-4, max_lr=learning_rate, cycle_momentum=False)
+    scheduler = CyclicLR(optimizer, base_lr=2e-3, max_lr=learning_rate, cycle_momentum=False)
 
     transform_for_train = transform.Compose([
         transform.RandomHorizontalFlip(p=0.5),
@@ -71,4 +87,5 @@ if __name__ == '__main__':
     calc_device = 'cuda' if torch.cuda.is_available() else 'cpu'
     net = VGG16(class_num=10).to(calc_device)  # 声明模型
     save_path = '../save_ckpt'
-    train(net, epochs=10, device=calc_device, save_root_path=save_path)
+    pre_train_model = '../pretrain_model/vgg16-397923af.pth'
+    train(net, epochs=10, device=calc_device, save_root_path=save_path, pre_train_model_path=pre_train_model)
